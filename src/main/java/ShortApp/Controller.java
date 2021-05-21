0260be17f07d812d525ff8d65ca0bafdde7f533c
Bürki Noé <noe@buerki.ch>
2021-05-21 18:12:08 +0200

    Changes and new Commands

--- src/main/java/ShortApp/Controller.java
+++ src/main/java/ShortApp/Controller.java
@@ -1,90 +1,86 @@
 package ShortApp;
 
-import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
-import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Locale;
 
 public class Controller {
-    private Label commandOutput;
+    private final Text commandOutput;
+    private final Stage parent;
 
-     public Controller(Label commandOutput){
-         this.commandOutput = commandOutput;
-     }
+    public Controller(Text commandOutput, Stage parent) {
+        this.commandOutput = commandOutput;
+        this.parent = parent;
+    }
 
-     public void handelInput(String input){
-         if(Regex("cmd*", input)){
-             exec(input);
-         } else if(Regex("google*", input)){
-             commandOutput.setText("GOOGLE DETECTED");
-         } else if(Regex("www*", input)){
-             commandOutput.setText("WEBSITE DETECTED");
-         } else if(Regex("ipconfig", input)){
-             exec(input);
+    public void handelInput(String input) {
+        if (input != "") {
+            new Thread(new Executor(this, input, commandOutput)).start();
+        }
+    }
 
-         }
-     }
+    public String exec(String command, boolean response) {
+        try {
+            String message = "";
+            Runtime rt = Runtime.getRuntime();
+            Process process = rt.exec(command);
 
-     public String exec(String command){
-         try {
-             String message = "";
-             Runtime rt = Runtime.getRuntime();
-             Process process = rt.exec(command);
+            if (response){
 
-             BufferedReader stdInput = new BufferedReader(new
-                     InputStreamReader(process.getInputStream()));
+                BufferedReader stdInput = new BufferedReader(new
+                        InputStreamReader(process.getInputStream()));
 
-             BufferedReader stdError = new BufferedReader(new
-                     InputStreamReader(process.getErrorStream()));
+                BufferedReader stdError = new BufferedReader(new
+                        InputStreamReader(process.getErrorStream()));
 
-             String s = null;
-             while ((s = stdInput.readLine()) != null) {
-                 message += s;
-             }
+                String s = null;
+                while ((s = stdInput.readLine()) != null) {
+                    message += s + "\n";
+                }
 
-             while ((s = stdError.readLine()) != null) {
-                 message += s;
-             }
-             return message;
-         } catch (IOException e){
-             JOptionPane.showMessageDialog(null, e.getMessage());
-             return "";
-         }
-     }
+                while ((s = stdError.readLine()) != null) {
+                    message += s + "\n";
+                }
+                return message;
+            } else {
+                return "";
+            }
+        } catch (IOException e) {
+            commandOutput.setText("Please Check Your Syntax");
+            return "";
+        }
+    }
 
-    public boolean Regex(String first, String second)
-    {
+    public boolean Regex(String first, String second) {
 
-        // If we reach at the end of both strings,
-        // we are done
         if (first.length() == 0 && second.length() == 0)
             return true;
 
-        // Make sure that the characters after '*'
-        // are present in second string.
-        // This function assumes that the first
-        // string will not contain two consecutive '*'
         if (first.length() > 1 && first.charAt(0) == '*' &&
                 second.length() == 0)
             return false;
 
-        // If the first string contains '?',
-        // or current characters of both strings match
         if ((first.length() > 1 && first.charAt(0) == '?') ||
                 (first.length() != 0 && second.length() != 0 &&
                         first.charAt(0) == second.charAt(0)))
             return Regex(first.substring(1),
                     second.substring(1));
 
-        // If there is *, then there are two possibilities
-        // a) We consider current character of second string
-        // b) We ignore current character of second string.
         if (first.length() > 0 && first.charAt(0) == '*')
             return Regex(first.substring(1), second) ||
                     Regex(first, second.substring(1));
         return false;
+    }
+
+    public void maximize() {
+        parent.toFront();
+        parent.requestFocus();
+    }
+
+    public void minimize() {
+        parent.toBack();
     }
 }
--- src/main/java/ShortApp/GUI.java
+++ src/main/java/ShortApp/GUI.java
@@ -1,21 +1,19 @@
 package ShortApp;
 
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class GUI extends Application {
     private Stage stage;
@@ -26,45 +24,59 @@ public class GUI extends Application {
     @Override
     public void start(Stage primaryStage) {
         stage = primaryStage;
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.setResizable(false);
         stage.setMinHeight(CONSTANTS.HEIGHT);
         stage.setMaxWidth(CONSTANTS.WIDTH);
-
+        stage.getIcons().add(new Image("icon/icon.ico"));
         TextField commandInput = new TextField();
         commandInput.setMinSize(CONSTANTS.WIDTH, 70);
+        commandInput.setMaxSize(CONSTANTS.WIDTH, 70);
         commandInput.setBackground(new Background(new BackgroundFill(Color.hsb(220, 0.51, 0.34), CornerRadii.EMPTY, Insets.EMPTY)));
         commandInput.setLayoutX(0);
         commandInput.setLayoutY(0);
         commandInput.setStyle("-fx-text-inner-color: white;");
-        commandInput.setFont(Font.font("Calibri", FontWeight.SEMI_BOLD, 30));
+        commandInput.setFont(Font.loadFont(getClass().getResource("/font/LexendDeca-Regular.ttf").toExternalForm(), 40));
         commandInput.setPadding(new Insets(5, 10, 5, 10));
-        commandInput.setOnKeyPressed(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent event) {
-                if (event.getCode().equals(KeyCode.ENTER)){
-                    controller.handelInput(commandInput.getText());
-                    commandInput.setText("");
-                }
+        commandInput.setOnKeyPressed(event -> {
+            if (event.getCode().equals(KeyCode.ENTER)) {
+                controller.handelInput(commandInput.getText());
+                commandInput.setText("");
             }
         });
 
-        Label commandOutput = new Label();
-        commandOutput.setMinSize(CONSTANTS.WIDTH, CONSTANTS.HEIGHT - commandInput.getMinHeight());
-        commandOutput.setBackground(new Background(new BackgroundFill(Color.hsb(220, 0.51, 0.5), CornerRadii.EMPTY, Insets.EMPTY)));
+        Text commandOutput = new Text();
+        commandOutput.minWidth(CONSTANTS.WIDTH);
+        commandOutput.minHeight(CONSTANTS.HEIGHT - commandInput.getMinHeight());
+        commandOutput.setFill(Color.WHITE);
         commandOutput.setLayoutX(0);
-        commandOutput.setLayoutY(commandInput.getMinHeight());
-        commandOutput.setTextFill(Color.WHITE);
-        commandOutput.setFont(Font.font("Calibri", FontWeight.SEMI_BOLD, 20));
-        commandOutput.setPadding(new Insets(5, 10, 5, 10));
-        commandOutput.setWrapText(true);
+        commandOutput.setLayoutY(0);
+        commandOutput.setFont(Font.loadFont(getClass().getResource("/font/LexendDeca-Regular.ttf").toExternalForm(), 30));
+        commandOutput.setWrappingWidth(CONSTANTS.WIDTH - 50);
 
-        controller =  new Controller(commandOutput);
+        StackPane stackPane = new StackPane();
+        stackPane.setMinSize(CONSTANTS.WIDTH, CONSTANTS.HEIGHT - commandInput.getMinHeight() - 2);
+        stackPane.setBackground(new Background(new BackgroundFill(Color.hsb(220, 0.51, 0.5), CornerRadii.EMPTY, Insets.EMPTY)));
+        stackPane.getChildren().addAll(commandOutput);
+
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setMinSize(CONSTANTS.WIDTH, CONSTANTS.HEIGHT - commandInput.getMinHeight());
+        scrollPane.setMaxSize(CONSTANTS.WIDTH, CONSTANTS.HEIGHT - commandInput.getMinHeight());
+        scrollPane.setLayoutX(0);
+        scrollPane.setLayoutY(commandInput.getMinHeight());
+        scrollPane.setBorder(Border.EMPTY);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setContent(stackPane);
+        scrollPane.setBackground(new Background(new BackgroundFill(Color.hsb(220, 0.51, 0.5), CornerRadii.EMPTY, Insets.EMPTY)));
+
+        controller = new Controller(commandOutput, stage);
 
         AnchorPane pane = new AnchorPane();
-        pane.getChildren().addAll(commandInput, commandOutput);
+        pane.getChildren().addAll(commandInput, scrollPane);
 
         root = new Scene(pane);
         stage.setScene(root);
         stage.show();
+
     }
 }
